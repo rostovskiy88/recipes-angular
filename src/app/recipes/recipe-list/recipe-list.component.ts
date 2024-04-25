ef274072e0be728a51ca12ebed8aaecab8dc6f6a
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2024-04-25 23:10:19 +0300

    add recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2023/08/chole-recipe.jpg'),
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2023/08/chole-recipe.jpg'),
+  ];
 
 }
